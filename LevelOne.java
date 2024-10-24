698c2e164b91d79492c3041626f8d5aaf2dba5ac
ryxnh <349739326@gapps.yrdsb.ca>
2024-10-24 14:18:36 -0400

    changed some images
    adding fade in abstract class and textbox actor classes next

--- LevelOne.java
+++ LevelOne.java
@@ -8,15 +8,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class LevelOne extends World
 {
-
-    /**
-     * Constructor for objects of class LevelOne.
-     * 
-     */
+    
     public LevelOne()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+    
+        setBackground(new GreenfootImage("map1bg.png")); 
         
         PlayerInput playerInput = new PlayerInput(); 
         addObject(playerInput, 300, 200); 
