2301e6ffa9e33f2ee11d1c5faeafd1f1739628c1
ryxnh <349739326@gapps.yrdsb.ca>
2024-10-24 12:42:15 -0400

    added player input code (test)

--- LevelOne.java
+++ LevelOne.java
@@ -16,6 +16,10 @@ public class LevelOne extends World
     public LevelOne()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        
+        PlayerInput playerInput = new PlayerInput(); 
+        addObject(playerInput, 300, 200); 
+        
     }
 }
