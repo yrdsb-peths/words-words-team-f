bb1ce9b348c7576bf00c9d962493d37779cc000a
Nicholas-lh <350373023@gapps.yrdsb.ca>
2024-10-24 13:15:55 -0400

    LevelOne is made (base structures)

--- LevelOne.java
+++ LevelOne.java
@@ -1,21 +1,40 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList; 
 
-/**
- * Write a description of class LevelOne here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class LevelOne extends World
 {
-
-    /**
-     * Constructor for objects of class LevelOne.
-     * 
-     */
+    private boolean gameStarted = false;
+    private ArrayList<String> ingredients = new ArrayList<String>();
+    private String[] userAnswers = new String[3]; 
+    private int currentAnswerIndex = 0; 
+    
     public LevelOne()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);         
+        showText("this is level one", 300,200);
+        //setImage(image);
+        //setWords
+        ingredients.add("tomato");
+        ingredients.add("cucumber");
+        ingredients.add("carrot");
+    }
+    
+    public void act()
+    {
+        if(!gameStarted)
+        {
+            //loading screen
+            if (Greenfoot.isKeyDown("enter"))
+            {
+                showText("", 300, 200);  // Clear the intro text
+                gameStarted = true;
+                showWords();
+            }
+        }
+    }
+    private void showWords()
+    {
+        showText("[" + ingredients.get(0) + "] [" + ingredients.get(1) + "] [" + ingredients.get(2) + "]", 300,200);
     }
 }
