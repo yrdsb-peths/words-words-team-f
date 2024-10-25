8c97dc089aea0e12dd5834aed907edff83193e85
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-10-25 13:18:03 -0400

    remove gameWorld added playerInput

--- PlayerInput.java
+++ PlayerInput.java
@@ -1,52 +1,72 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class PlayerInput extends Actor {
+    private String userInput = "";
+    private ArrayList<String> words;
+    private ArrayList<String> possibleKeys;
+    private int timeRemaining; // Time in frames to memorize
+    private boolean isMemorizing = true;
+    private boolean wordsShown = false;
+    
+    public PlayerInput(int timeRemaining, ArrayList<String> words) {
+        this.timeRemaining = timeRemaining;
+        this.words = words;
+        setImage((GreenfootImage)null);
+    }
 
-/**
- * Write a description of class PlayerInput here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class PlayerInput extends Actor
-{
-    private String inputText = ""; 
-    private GreenfootImage displayImage; 
-    
-    public PlayerInput()
-    {
-        displayImage = new GreenfootImage(200, 50); 
-        updateDisplay(); 
-        
-    }
-    
-    private void updateDisplay()
-    {
-        displayImage.clear();
-        
-        displayImage.setFont(new Font("Arial", 24));
-        displayImage.setColor(Color.BLACK);
-        
-        displayImage.drawString(inputText, 10, 30);
-        
-        setImage(displayImage);
-    }
-    
-    
-    public void act()
-    {
-        String key = Greenfoot.getKey(); 
-        
-        if(key != null)
-        {
-            if(key.equals("backspace") && inputText.length() > 0) 
-            {
-                inputText = inputText.substring(0, inputText.length() - 1);
-            }
-            else if (key.length() == 1)
-            {
-                inputText += key;
-            }
-            
-            updateDisplay(); 
+    public void act() {
+        if (!wordsShown && getWorld() != null) {
+            showWords();
+            wordsShown = true;
         }
+        if (isMemorizing) {
+            updateTimer();
+        } else {
+            handleUserTyping();
+        }
+    }
+
+    private void showWords() {
+        StringBuilder displayText = new StringBuilder("Memorize these words:\n");
+        for (String word : words) {
+            displayText.append(word).append(" ");
+        }
+        getWorld().showText(displayText.toString(), 300, 200);
+    }
+
+    private void updateTimer() {
+        if (timeRemaining > 0) {
+            timeRemaining--;  // Countdown
+            int secondsRemaining = timeRemaining / 60;
+            getWorld().showText("Time to memorize: " + secondsRemaining + "s", 300, 50);  // Show remaining time
+        } else {
+            isMemorizing = false;
+            getWorld().showText("", 300, 50); // Clear the timer
+            getWorld().showText("", 300, 200); // Clear the words
+        }
+    }
+
+    private void handleUserTyping() {
+        possibleKeys = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "space"));
+        for (String key : possibleKeys) {
+            if (Greenfoot.isKeyDown(key)) {
+                if (key.equals("space")) {
+                    userInput += " ";
+                } else {
+                    userInput += key;
+                }
+                Greenfoot.delay(5);
+            }
+        }
+        if (Greenfoot.isKeyDown("backspace") && userInput.length() > 0) {
+            userInput = userInput.substring(0, userInput.length() - 1);
+            Greenfoot.delay(5);
+        }
+        if (Greenfoot.isKeyDown("enter")) {
+            userInput = "";
+            Greenfoot.delay(5);
+        }
+        getWorld().showText("Typed: " + userInput, 300, 350);
     }
 }
