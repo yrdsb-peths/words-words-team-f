616de702f61657452dc59d736316e220df6f549f
ryxnh <349739326@gapps.yrdsb.ca>
2024-10-25 23:14:47 -0400

    deleted old classes

--- MyWorld.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;
-
-public class MyWorld extends World {
-    public MyWorld() {
-        super(600, 400, 1);
-        CountdownTimer timer = new CountdownTimer(30); // 30-second countdown
-        addObject(timer, 100, 50);
-    }
-}
