2a72e76df09588594d5a63689cb66634dd78f4e6
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-11-02 12:47:47 -0400

    adding instruction screen background

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -16,14 +16,44 @@ public class InstructionScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         this.levelName = levelName;
-        
+        setBackground(new GreenfootImage("InstructionScreen.jpg"));
+        showInstructions();
+        addStartButton();
     }
-    public void act()
+    public void showInstructions()
     {
-        
+        String instructions = "";
+        switch(levelName)
+        {
+            case "Level 1":
+                instructions = "Level 1: Memorize the displayed words.\nAfter the timer ends, input the words in the correct order.";
+                break;
+            case "Level 2":
+                instructions = "Level 2: More words to memorize and less time!\nInput the words in the correct order after memorization.";
+                break;
+            case "Level 3":
+                instructions = "Level 3: Challenge mode! Be quick and precise.\nMemorize the words and type them in order correctly.";
+                break;
+        }
     }
-    public void showIntructions()
+    private void addStartButton()
     {
-        
+        Button startButton = new Button(() -> startLevel(), "startButton.png", "startButton.png");
+        addObject(startButton, getWidth() / 2, getHeight() - 50);
+    }
+    private void startLevel()
+    {
+        switch (levelName)
+        {
+            case "Level 1":
+                Greenfoot.setWorld(new LevelOne());
+                break;
+            case "Level 2":
+                Greenfoot.setWorld(new LevelTwo());
+                break;
+            case "Level 3":
+                Greenfoot.setWorld(new LevelThree());
+                break;
+        }
     }
 }
