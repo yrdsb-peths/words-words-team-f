e546987acecf1f4367ec6602bd8e02b44473b87b
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-11-05 12:38:14 -0500

    Update InstructionWorld.java

--- InstructionWorld.java
+++ InstructionWorld.java
@@ -8,14 +8,47 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class InstructionWorld extends World
 {
-
-    /**
-     * Constructor for objects of class InstructionWorld.
-     * 
-     */
+    private String levelName;
+    private GreenfootImage background;
+    
+    private int scrollX;
+    private int counter;
+    
     public InstructionWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        background = new GreenfootImage("InstructionScreen.png");
+    }
+    public void act()
+    {
+        scrollBackground();
+    }
+    private void scrollBackground()
+    {
+        counter++; // increase the counter
+        
+        //update scroll position every 5 frames
+        if(counter >= 5)
+        {
+            scrollX += 1;   //scroll speed
+            counter = 0; 
+        }
+        
+        GreenfootImage scrolledImage = new GreenfootImage(getWidth(), getHeight());
+        
+        int imageWidth = background.getWidth();
+        int imageHeight = background.getHeight();
+        
+        int x = scrollX % imageWidth; 
+        
+        //drawing the image twice to cover the empty areas during scrolling animation
+        scrolledImage. drawImage(background, -x, 0);
+        scrolledImage.drawImage(background, -x + imageWidth, 0); 
+        scrolledImage.drawImage(background, -x, 0); 
+        scrolledImage.drawImage(background, -x + imageWidth, 0);
+        
+        //set the background to the scrolled image
+        setBackground(scrolledImage); 
     }
 }
