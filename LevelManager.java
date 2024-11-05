e09761f33dcff5f7927de39cd690655e0d5fb94b
Nicholas-lh <350373023@gapps.yrdsb.ca>
2024-11-04 20:38:09 -0500

    made a list

---
+++ LevelManager.java
@@ -0,0 +1,30 @@
+import greenfoot.*;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Write a description of class LevelManager here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LevelManager  
+{
+    // instance variables - replace the example below with your own
+    /**
+     * Constructor for objects of class LevelManager
+     */
+    private static List<World> levels = new ArrayList<>(); //store levels as instances
+    private static int currentLevel = 0;
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    
+    public static World getCurrentLevel()
+    {
+        
+    }
+}
